6cae75ab9be9ce18cc38bd7ef017179f6896ea98
Jiulong Wang <jiulongw@gmail.com>
2015-11-01 20:23:08 -0800

    add mode indicator for zsh vi mode

--- .zsh/keys.zsh
+++ .zsh/keys.zsh
@@ -4,3 +4,12 @@ bindkey '^?' backward-delete-char
 bindkey '^h' backward-delete-char
 bindkey '^w' backward-kill-word
 
+function zle-line-init zle-keymap-select {
+    VIM_PROMPT="%{$fg_bold[yellow]%} [% NORMAL]%  %{$reset_color%}"
+    RPS1="${${KEYMAP/vicmd/$VIM_PROMPT}/(main|viins)/}"
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
+
