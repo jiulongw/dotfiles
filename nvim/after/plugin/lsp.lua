a682fcae4aa2ab88819e831bcf96f1e36854cf35
Jiulong Wang <jiulongw@gmail.com>
2025-06-12 14:26:13 -0700

    nvim: format on save only for golang

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -27,13 +27,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     if not client:supports_method('textDocument/willSaveWaitUntil')
         and client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-        buffer = args.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-        end,
-      })
+      local ft = vim.api.nvim_get_option_value('filetype', { buf = args.buf })
+      if ft == 'go' then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+          buffer = args.buf,
+          callback = function()
+            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+          end,
+        })
+      end
     end
   end
 })
